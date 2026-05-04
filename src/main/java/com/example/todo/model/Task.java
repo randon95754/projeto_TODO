fef790d2f469c0271bdff7161c6348b116137511
Randson Thiago Sales da Silva Lima <f209953@intrajus.tjrn>
2026-05-04 10:03:18 -0300

    c2

--- src/main/java/com/example/todo/model/Task.java
+++ src/main/java/com/example/todo/model/Task.java
@@ -1,13 +1,50 @@
 package com.example.todo.model;
+
 import jakarta.persistence.*;
 
-
 @Entity
-public class Task{
+public class Task {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
     private boolean completed;
+
+    // Construtor vazio obrigatório para o JPA
+    public Task() {
+    }
+
+    // Construtor opcional
+    public Task(String title, boolean completed) {
+        this.title = title;
+        this.completed = completed;
+    }
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
